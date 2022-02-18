b1719dcfcaa4e9f6b274257227b2a294469e6f86
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:23:46 -0500

    project model written

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,19 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+const get = async () => {
+    const data = await db('projects')
+    
+    return data
+}
+
+const post = async (project) => {
+    await db('projects').insert(project)
+    
+    return
+}
+
+module.exports = {
+    get,
+    post,
+}
