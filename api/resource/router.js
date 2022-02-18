47e69d14ae113b35749a72e73eed9441135e1e41
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:27:54 -0500

    resource router added

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,24 @@
 // build your `/api/resources` router here
+const express =  require('express')
+const Resource = require('./model')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = await Resource.get()
+        res.json(data)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const newResource = await Resource.post(req.body)
+        res.json(newResource)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
