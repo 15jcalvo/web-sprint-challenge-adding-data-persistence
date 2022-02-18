aabb2ff78c8f2f60df710374c4748a0ac2c0b057
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:27:18 -0500

    resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,19 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const get = async () => {
+    const data = await db('resources')
+    
+    return data
+}
+
+const post = async (resource) => {
+    await db('resources').insert(resource)
+    
+    return
+}
+
+module.exports = {
+    get,
+    post,
+}
