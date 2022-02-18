444a580820c5c437fc9c0a0c9b08f6b842303790
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:29:55 -0500

    task model added

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,19 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+const get = async () => {
+    const data = await db('tasks')
+    
+    return data
+}
+
+const post = async (task) => {
+    await db('tasks').insert(task)
+    
+    return
+}
+
+module.exports = {
+    get,
+    post,
+}
