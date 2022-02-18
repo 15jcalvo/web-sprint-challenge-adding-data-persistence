1ee24e9f47aa317455b3747071df85b04b163f60
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:20:02 -0500

    added tasks and project resources tables

--- data/migrations/20220218190623_tables.js
+++ data/migrations/20220218190623_tables.js
@@ -11,10 +11,24 @@ exports.up = async function(knex) {
             table.text('resource_name').notNullable().unique()
             table.text('resource_description')
         })
+        .createTable('tasks', table => {
+            table.increments('task_id')
+            table.text('task_description').notNullable()
+            table.text('task_notes')
+            table.boolean('task_completed').defaultTo(false)
+            table.integer('project_id').notNullable()
+        })
+        .createTable('project_resources', table => {
+            table.increments('assignment_id')
+            table.integer('resource_id')
+            table.integer('project_id')
+        })
 };
 
 exports.down = async function(knex) {
     await knex.schema
         .dropTableIfExists('projects')
         .dropTableIfExists('resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('project_resources')
 };
