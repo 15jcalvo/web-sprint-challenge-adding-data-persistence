5c8eb66692f1c89c10d38e18f58189423cd4d877
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:25:04 -0500

    project router written

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,24 @@
 // build your `/api/projects` router here
+const express =  require('express')
+const Project = require('./model')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = await Project.get()
+        res.json(data)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const newProject = await Project.post(req.body)
+        res.json(newProject)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
