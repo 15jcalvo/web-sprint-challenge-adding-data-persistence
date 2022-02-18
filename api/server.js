5b15cfd02561af6b56d931f6ec8a2f8de497c297
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:28:32 -0500

    resource router added

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 // build your server here and require it from index.js
 const express = require('express');
 const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
 
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/projects', projectRouter)
+server.use('/api/resources', resourceRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
