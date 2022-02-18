c9aa024a479b406bc78c52bac3729a6ffc9baa6c
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:09:34 -0500

    added reosurces table

--- data/migrations/20220218190623_tables.js
+++ data/migrations/20220218190623_tables.js
@@ -5,10 +5,16 @@ exports.up = async function(knex) {
             table.text('project_name').notNullable().unique()
             table.text('project_description')
             table.boolean('project_completed').defaultTo(false)
-    })
+        })
+        .createTable('resources', table => {
+            table.increments('resource_id')
+            table.text('resource_name').notNullable().unique()
+            table.text('resource_description')
+        })
 };
 
 exports.down = async function(knex) {
     await knex.schema
         .dropTableIfExists('projects')
+        .dropTableIfExists('resources')
 };
