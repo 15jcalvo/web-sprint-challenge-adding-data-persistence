c0951de68ef48f7c35d4815c1c82468c7f4df9bc
James_Calvo <15jcalvo@gmail.com>
2022-02-18 14:31:13 -0500

    task router added

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,25 @@
 // build your `/api/tasks` router here
+const express =  require('express')
+const Task = require('./model')
+const router = express.Router()
+const { checkProjectId } = require('./middleware')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = await Task.get()
+        res.json(data)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', checkProjectId, async (req, res, next) => {
+    try{
+        const newTask = await Task.post(req.body)
+        res.json(newTask)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
